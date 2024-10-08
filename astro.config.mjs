9a3b6969c238e9b47c39275d2c87a4f068efc8d7
scul0405 <90521402+scul0405@users.noreply.github.com>
2024-10-09 01:33:29 +0700

    update blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import { SITE_METADATA } from "./src/consts.ts";
 import metaTags from "astro-meta-tags";
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 import robotsTxt from "astro-robots-txt";
 
@@ -16,5 +18,8 @@ export default defineConfig({
   },
   prefetch: true,
   site: SITE_METADATA.siteUrl,
-  integrations: [mdx(), sitemap(), tailwind(), solidJs(), metaTags(), robotsTxt()]
+  integrations: [mdx({
+    remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex]
+  }), sitemap(), tailwind(), solidJs(), metaTags(), robotsTxt()]
 });
